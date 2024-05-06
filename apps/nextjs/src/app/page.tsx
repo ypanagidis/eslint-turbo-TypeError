72d8be868151ea1af6cfc6a26f9832bcc2b26c13
Yiannis Panagidis <ypanagidis@gmail.com>
2024-05-06 20:15:45 +0300

    julius is the goat and fixed it in 6 minutes

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
   const test = { someValue: 1 };
   useEffect(() => {
     if (test.someValue === 1) alert("1");
-    eslint-disable-next-line react-hooks/exhaustive-deps
+    //* eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <h1>Test</h1>;
 }
