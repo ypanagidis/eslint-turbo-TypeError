2da30bd4a0d226de076b572a18fe63cd5f6df4b9
Yiannis Panagidis <ypanagidis@gmail.com>
2024-05-06 20:10:52 +0300

    julius is the goat and got the issue and fixed it in 6 minutes

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
+// /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { useEffect } from "react";
@@ -9,6 +9,7 @@ export default function HomePage() {
   const test = { someValue: 1 };
   useEffect(() => {
     if (test.someValue === 1) alert("1");
+    eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <h1>Test</h1>;
 }
